97b254c2168b47611b9cf95623f720bca335df27
Marco <maransi>
2020-07-19 20:17:01 -0300

    Initial version

---
+++ demo-boot-jquery-datatable/src/test/java/com/maransi/DemoBootJqueryDatatableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maransi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBootJqueryDatatableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
