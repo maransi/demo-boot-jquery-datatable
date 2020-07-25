0f0b5ceed53647a168b0b4a2605a5495d1af2cd9
Marco <maransi>
2020-07-25 02:39:16 -0300

    Versão funcional

--- demo-boot-jquery-datatable/src/main/java/com/maransi/controller/PessoaResource.java
+++ demo-boot-jquery-datatable/src/main/java/com/maransi/controller/PessoaResource.java
@@ -1,10 +1,11 @@
 package com.maransi.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,17 +20,28 @@ public class PessoaResource {
 
 	@Autowired
 	PessoaService pessoaService;
-	
-	@RequestMapping( method=RequestMethod.GET, 
-					produces=MediaType.APPLICATION_JSON_VALUE ,
-					headers = "Accept=*/*")
+
+	@RequestMapping(// value="/nome",
+			method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, headers = "Accept=*/*")
 //	@GetMapping(produces = "application/json")	
-	public @ResponseBody List<Pessoa> findByNome(  Pessoa pessoa ){
-		List<Pessoa> pessoas = pessoaService.findByNome(pessoa.getNome());
-		
+	public @ResponseBody List<Pessoa> findPessoa(Pessoa pessoa) {
+		List<Pessoa> pessoas = null;
+
+		if (!pessoa.getNome().isEmpty()) {
+			pessoas = pessoaService.findByNome(pessoa.getNome());
+		} else if (!pessoa.getCpf().isEmpty()) {
+			pessoas = new ArrayList<Pessoa>();
+
+			Optional<Pessoa> p = pessoaService.findByCpf(pessoa.getCpf());
+
+			if (p.isPresent()) {
+				pessoas.add(p.get());
+			}
+		}
+
 		System.out.println(pessoas.toString());
-		
+
 		return pessoas;
 	}
-	
+
 }
